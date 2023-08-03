f9626e175e4941e9345d3e13b7e1a6d10e6fe6ae
19Edwin92 <b_a_s_e@naver.com>
2023-08-03 20:22:57 +0900

    [Socket.io] 웹소켓 채팅을 위한 node 임시서버 구축 수정(1)

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-const experss = require("express")
-const app = experss()
-const http = require("http")
-const cors = require("cors")
-const {Server} = require("socket.io")
+const experss = require("express");
+const app = experss();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors : {
-    origin : "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-// (1) socket.connection 연결하기 
-io.on("connection", (socket)=> {
-  console.log(socket.io);
+// (1) socket.connection 연결하기 // 연결되면, 연결된 정보를 노드콘솔에 기록하기
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+  console.log("Token received:", socket.handshake.auth.authorization); // 사용자정보 -> DB
 
-          // (3) socket.emit 내용보내기
+  // (1-1) 룸 정보 연결하기 
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
 
-          // (4) socket.on 내용보내기
+  // (1-2) socket.emit 내용보내기
+  socket.on("send_message", (data) => {
+    socket.to(data.chatNum).emit("receive_message", {...data, id:socket.id});
+  });
 
-
-          
-
-  // (2) socket.disconnected 연결해제
+    // (1-3) socket.disconnected 연결해제
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
-  })
+  });
+});
 
-})
-
-server.listen(3001, ()=> {
+server.listen(3001, () => {
   console.log("SERVER RUNNING");
-})
+});
+
+
+
+
